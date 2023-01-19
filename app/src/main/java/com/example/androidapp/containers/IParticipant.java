7a922edc1eb69d3fa79113871eeada470355303f
aprax11 <jashaspringmann@web.de>
2023-01-19 21:59:10 +0100

    fixed layout

--- app/src/main/java/com/example/androidapp/containers/IParticipant.java
+++ app/src/main/java/com/example/androidapp/containers/IParticipant.java
@@ -1,13 +1,23 @@
 package com.example.androidapp.containers;
 
-import java.io.Serializable;
 
-public interface IParticipant extends Serializable {
+public interface IParticipant{
+    /*
+    gets the score of the participant for a certain challenge
+    @return String score
+     */
     String getScore();
-
+    /*
+    sets the score of the participant for a certain challenge
+     */
     void setScore(String score);
-
+    /*
+    gets the name of the participant for a certain challenge
+    @return String name
+     */
     String getName();
-
+    /*
+    sets the name of the participant for a certain challenge
+     */
     void setName(String name);
 }
