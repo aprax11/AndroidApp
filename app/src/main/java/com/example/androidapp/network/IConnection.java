cc43d26bae4fa759722f20a951ef3d70f2c29be2
aprax11 <jashaspringmann@web.de>
2023-01-19 22:26:44 +0100

    fixed layout

--- app/src/main/java/com/example/androidapp/network/IConnection.java
+++ app/src/main/java/com/example/androidapp/network/IConnection.java
@@ -3,5 +3,10 @@ package com.example.androidapp.network;
 import java.io.IOException;
 
 public interface IConnection {
+    /*
+    takes a address for the HttpURLConnection and a url encoded message String
+    opens a connection with the urlString and writes the message via POST request method
+    @return String response
+     */
     String startRequest(String urlString, String urlEncoded) throws IOException;
 }
