c147040d9a0d2f2f34920a369cd20729842bd905
aprax11 <jashaspringmann@web.de>
2023-01-19 21:55:08 +0100

    fixed layout

--- app/src/main/java/com/example/androidapp/containers/IChallenge.java
+++ app/src/main/java/com/example/androidapp/containers/IChallenge.java
@@ -1,15 +1,32 @@
 package com.example.androidapp.containers;
 
 public interface IChallenge {
+    /*
+    gets the name of the challenge that is represented by this object
+    @return String name
+     */
     String getName();
-
+    /*
+    sets the name of the challenge that is represented by this object
+    */
     void setName(String name);
-
+    /*
+    gets the description of the challenge that is represented by this object
+    @return String description
+     */
     String getDescription();
-
+    /*
+    sets the description of the challenge that is represented by this object
+     */
     void setDescription(String description);
-
+    /*
+    gets the id of the challenge that is represented by this object
+    @return int name
+     */
     String getId();
-
+    /*
+    gets the id of the challenge that is represented by this object
+    this is mainly used for testing
+     */
     void setId(String id);
 }
